92ca97d9d7704527ef8e1ea81e1afac6d13a0fb1
Samuel Jenness <sjenness@uw.edu>
2015-04-07 10:33:16 -0700

    Update basic example files

--- inst/examples/master.sh
+++ inst/examples/master.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-qsub -t 1-4 -v SIMNO=001 runsim.sh
-qsub -t 1-4 -v SIMNO=002 runsim.sh
+qsub -v SIMNO=001 runsim.sh
--- inst/examples/sim001.R
+++ inst/examples/sim001.R
@@ -1,16 +1,20 @@
+library(EpiModel.hpc)
 
-suppressPackageStartupMessages(library(EpiModelHIV))
+nw <- network.initialize(n = 10000, directed = FALSE)
+formation <- ~ edges
+target.stats <- 5000
+dissolution <- ~ offset(edges)
+duration <- 50
+coef.diss <- dissolution_coefs(dissolution, duration)
 
-args <- commandArgs(FALSE)
-args <- args[length(args)]
-fsimno <- sub("-","",args)
-print(fsimno)
+est <- netest(nw, formation, dissolution,
+              target.stats, coef.diss)
 
-param <- param.hiv()
-init <- init.hiv(i.prev.male = 0.01, i.prev.feml = 0.01)
-control <- control.hiv(simno = fsimno,
-                       nsteps = 52*100,
-                       nsims = 15,
-                       ncores = 15)
+param <- param.net(inf.prob = 0.01)
+init <- init.net(i.num = 50)
+control <- control.net(type = "SI", nsteps = 1000, verbose = FALSE,
+                       par.type = "mpi", nsims = 100, ncores = 100)
 
-runsimHPC("est/est.rda", param, init, control)
+sim <- netsim_parallel(est, param, init, control)
+
+save(sim, file = "sim.rda")
