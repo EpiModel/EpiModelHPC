23f4c8c6459b4ef97ae83ce71065780b2865107a
Samuel Jenness <sjenness@uw.edu>
2015-04-20 13:20:14 -0700

    Clean up save_cpdata

--- R/save_cpdata.R
+++ R/save_cpdata.R
@@ -2,22 +2,12 @@
 #' @export
 save_cpdata <- function(dat, at) {
 
-  if (!is.null(dat$control$save.int)) {
-    if (!is.null(dat$control$ncores) && dat$control$ncores > 1) {
-      if (at %% dat$control$save.int == 0) {
-        currsim <- dat$control$currsim
-        simno <- dat$control$simno
-        fn <- paste0("data/sim", simno, "/sim", currsim, ".cp.rda")
-        x <- dat
-        save(x, file = fn)
-      }
-    } else {
-      if (at %% dat$control$save.int == 0) {
-        fn <- paste0("sim.cp.rda")
-        x <- dat
-        save(x, file = fn)
-      }
-    }
+  if (!is.null(dat$control$save.int) && at %% dat$control$save.int == 0) {
+    currsim <- dat$control$currsim
+    simno <- dat$control$simno
+    fn <- paste0("data/sim", simno, "/sim", currsim, ".cp.rda")
+    x <- dat
+    save(x, file = fn)
   }
   
 }
