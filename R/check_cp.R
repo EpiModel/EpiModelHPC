86fc4504b72400bc40ffeaf6ce957722bba0db69
Samuel Jenness <sjenness@uw.edu>
2015-04-20 11:12:11 -0700

    Fix up check_cp

--- R/check_cp.R
+++ R/check_cp.R
@@ -5,25 +5,16 @@ check_cp <- function(simno) {
   out <- FALSE
   goodFile <- FALSE
   
-  dirname.top <- paste0("data/sim", simno)
-  dirname.bot <- paste0("data/sim", gsub(".ext[1-9]", "", simno))
+  dirname <- paste0("data/sim", simno)
   
-  if (file.exists(dirname.top) & file.exists(dirname.bot)) {
-    dirname <- dirname.top
-  } else if (!file.exists(dirname.top) & file.exists(dirname.bot)) {
-    dirname <- dirname.bot
-  } else {
+  if (file.exists(dirname) == FALSE) {
     return(NULL)
   }
   
   fn <- list.files(path = dirname, pattern = "*.cp.rda", full.names = TRUE)
-  if (length(fn) == 0) {
-    dirname <- dirname.bot
-    fn <- list.files(path = dirname, pattern = "*.cp.rda", full.names = TRUE)
-  }
   if (length(fn) > 0) {
     a <- unname(sapply(fn, function(x) file.info(x)$size))
-    goodFile <- ifelse(all(a > (mean(a) - 1000000)), TRUE, FALSE)
+    goodFile <- ifelse(all(a > (mean(a) - mean(a) * 0.5)), TRUE, FALSE)
   }
   
   if (goodFile == TRUE) {
