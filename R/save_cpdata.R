6d33971c503658bc9b401105063c21a9cf57cd9f
Samuel Jenness <sjenness@uw.edu>
2015-04-20 13:20:33 -0700

    Add docs for save_cpdata

--- R/save_cpdata.R
+++ R/save_cpdata.R
@@ -1,4 +1,18 @@
 
+#' @title Saves for Network Simulation Rdata Files for Checkpointing
+#'
+#' @description Module to save simulation data from stochastic network models
+#'              to disk at specified time intervals.
+#'
+#' @param dat A master data object used in models simulated with \code{netsim}.
+#' @param at Current time step
+#' 
+#' @details
+#' This module saves data to a standardized location with standardized file names
+#' for the purposes of checkpointing. This is intended to be used when running
+#' these simulations with \code{\link{netsim_hpc}}, and will be automatically
+#' inserted into the workflow when this is done.
+#' 
 #' @export
 save_cpdata <- function(dat, at) {
 
