3bfec71f25d47377765d174a0e20a5e52e903e20
Samuel Jenness <sjenness@uw.edu>
2015-05-18 12:55:46 -0700

    Add qsub master function

---
+++ R/qsub_master.R
@@ -0,0 +1,56 @@
+
+#' @title Create qsub Bash Shell Script Parameter Combination
+#'
+#' @description Creates a master-level qsub script given a set of parameter
+#'              combinations implied by one or more parameters passed in.
+#'
+#' @param outfile Name of the output bash shell script file. If \code{""}, then
+#'        will print to console.
+#' @param runsimfile Name of the bash shell script file that contains the R batch
+#'        commands
+#' @param simno.start Starting number for the \code{SIMNO} variable.
+#' @param nsubjobs Number of sub/array jobs to run per simulation set.
+#' @param backfill If \code{TRUE}, use the backfill queue to submit jobs.
+#' @param email If \code{TRUE}, send email on job termination or completion.
+#' @param vars A list of parameters with varying values (see example).
+#'
+#' @export
+#'
+#' @examples
+#' vars <- list(A = 1:10, B = seq(0.5, 1.5, 0.5))
+#' qsub_master(vars = vars, outfile = "")
+#'
+qsub_master <- function(outfile = "master.sh",
+                        runsimfile = "runsim.sh",
+                        simno.start = 1,
+                        nsubjobs = 4,
+                        backfill = TRUE,
+                        email = TRUE,
+                        vars) {
+
+  grd.temp <- do.call("expand.grid", vars)
+  SIMNO <- simno.start:(simno.start + nrow(grd.temp) - 1)
+  grd <- data.frame(SIMNO, grd.temp)
+
+  backfill.ch <- ifelse(backfill == TRUE, "-q bf", "")
+  email.ch <- ifelse(email == FALSE, "-m n", "-m ae")
+  if (nsubjobs > 1) {
+    nsubjobs.ch <- paste("1", nsubjobs, sep = "-")
+  } else {
+    nsubjobs.ch <- "1 "
+  }
+
+  cat("#!/bin/bash\n", file = outfile)
+  for (i in 1:nrow(grd)) {
+    v.args <- NA
+    for (j in 1:ncol(grd)) {
+      v.args[j] <- paste0(names(grd)[j], "=", grd[i,j])
+    }
+    v.args <- paste(v.args, collapse = ",")
+
+    cat("\nqsub", backfill.ch, "-t", nsubjobs.ch, email.ch, "-v", v.args, runsimfile,
+        file = outfile, append = TRUE)
+  }
+  cat("\n", file = outfile, append = TRUE)
+
+}
