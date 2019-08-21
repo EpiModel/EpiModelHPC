e6b9ffce52f306e465f7f6b8ea271918e85cadec
smjenness <smjenness@gmail.com>
2019-08-21 16:56:37 -0400

    Update slurm taks per node env var

--- R/pull_env_vars.R
+++ R/pull_env_vars.R
@@ -18,7 +18,7 @@
 #' @examples
 #' Sys.setenv("SIMNO"=1000)
 #' Sys.setenv("SLURM_ARRAY_TASK_ID"=4)
-#' Sys.setenv("SLURM_NTASKS_PER_NODE"=4)
+#' Sys.setenv("SLURM_TASKS_PER_NODE"=4)
 #' Sys.setenv("NJOBS"=10)
 #' Sys.setenv("NSIMS"=100)
 #' 
@@ -59,7 +59,7 @@ pull_env_vars <- function(standard.vars = TRUE,
     } else {
       assign("fsimno", 1L, pos = 1)
     }
-    ncores <- as.numeric(Sys.getenv("SLURM_NTASKS_PER_NODE"))
+    ncores <- as.numeric(Sys.getenv("SLURM_TASKS_PER_NODE"))
     if (!is.na(ncores)) {
       assign("ncores", ncores, pos = 1)
     } else {
