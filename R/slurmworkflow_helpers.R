e84420782117a6408fc0f1efa3b006215c3cce0f
Adrien Le Guillou <git@aleguillou.org>
2022-04-29 14:21:13 +0200

    library call update

--- R/slurmworkflow_helpers.R
+++ R/slurmworkflow_helpers.R
@@ -137,12 +137,12 @@ step_tmpl_netsim_scenarios <- function(est, param, init, control,
                                        setup_lines = NULL,
                                        max_array_size = NULL) {
 
-  libraries <- c("slurmworkflow", "EpiModelHPC", libraries)
+  libraries <- as.list(c("slurmworkflow", "EpiModelHPC", libraries))
 
   n_batch <- ceiling(n_rep / n_cores)
 
   inner_fun <- function(scenario, batch_num) {
-    lapply(as.list(libraries), library)
+    lapply(libraries, library)
 
     if (!fs::dir.exists(output_dir))
       fs::dir.create(output_dir, recursive = TRUE)
